03e5c147bab90d8b4317fbd1b608bda9c546e6f7
RMS <reishmax@gmail.com>
2024-01-13 15:22:34 +0600

    added basic template for heading and progressbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import { Suspense } from 'preact/compat'
-import UserCount from 'components/UserCount'
+import textData from 'mock-data/text-data'
 
 export default function () {
+  const { title, description } = textData
+
+  const { companyRaised, companyRequired } = {
+    companyRaised: 2450,
+    companyRequired: 10000,
+  }
+  const companyRaisedPercentage = (
+    (companyRaised / companyRequired) *
+    100
+  ).toFixed(1)
+
   return (
-    <div className="">
-      <h1 className="font-title">Frontend template</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <UserCount />
-      </Suspense>
+    <div className="container p-8">
+      <section className="prose max-w-prose">
+        <h1 className="font-title text-6xl mb-4 tracking-wide ">{title}</h1>
+        <div className="flex items-center font-body font-medium text-xl">
+          <div className="flex flex-col">
+            <span>{companyRaised}$</span>
+            <span>{companyRaisedPercentage}%</span>
+          </div>
+          <progress
+            className="progress progress-primary mx-2"
+            value={companyRaised}
+            max={companyRequired}
+          ></progress>
+          <span className="">{companyRequired}$</span>
+        </div>
+        {description.split('\n').map((paragraph) => (
+          <p className="font-body text-lg mb-2 last:mb-0 ">
+            {paragraph.trim()}
+          </p>
+        ))}
+      </section>
     </div>
   )
 }
---
+++ src/mock-data/text-data.ts
@@ -0,0 +1,14 @@
+const data = {
+  title: "Medito's Mindful Mastery: Elevate Your Practice",
+  description: `🌟 Welcome to "Medito's Mindful Mastery," a groundbreaking project that's about to revolutionize the way you experience meditation! 🌟
+
+  At Medito, we believe that everyone deserves access to the profound wisdom of meditation masters. That's why we're launching an exciting new series of expert-led meditation masterclasses, and we need YOUR support to make it happen!
+
+  Imagine getting exclusive insights and practical tips from meditation gurus around the globe, all from the comfort of your own space. With "Medito's Mindful Mastery," you can do just that! These masterclasses are designed to help you take your meditation practice to the next level, offering a unique blend of ancient techniques and contemporary approaches.
+
+  But we can't do it without YOU. Your contribution will not only grant you VIP access to these enlightening sessions but will also empower us to bring these teachings to a wider audience. Together, let's create a community where mindfulness isn't just a practice—it's a way of life.
+
+  Join us on this exciting journey, and let's make mindful mastery accessible to everyone, one meditation at a time. Pledge your support today and be a part of the meditation revolution! 🌈🧘‍♀️🚀`,
+}
+
+export default data
