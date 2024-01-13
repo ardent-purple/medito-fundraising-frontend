fa08351041dff742ffc3955963489b5dd9571fd5
RMS <reishmax@gmail.com>
2024-01-13 17:28:25 +0600

    added Q&A section + feedback form with re-captcha

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import ReCAPTCHA from 'react-google-recaptcha'
+
+import { createRef } from 'preact'
 import textData from 'mock-data/text-data'
 
 export default function () {
-  const { title, description } = textData
+  const { title, description, questions } = textData
 
   const { companyRaised, companyRequired } = {
     companyRaised: 2450,
@@ -12,10 +15,12 @@ export default function () {
     100
   ).toFixed(1)
 
+  const captchaRef = createRef<ReCAPTCHA>()
+
   return (
-    <div className="container p-8">
-      <section className="prose max-w-prose">
-        <h1 className="font-title text-6xl mb-4 tracking-wide ">{title}</h1>
+    <div className="container p-8 bg-slate-900">
+      <section className="prose max-w-prose mb-4">
+        <h1 className="font-title text-6xl tracking-wide">{title}</h1>
         <div className="flex items-center font-body font-medium text-xl">
           <div className="flex flex-col">
             <span>{companyRaised}$</span>
@@ -29,10 +34,90 @@ export default function () {
           <span className="">{companyRequired}$</span>
         </div>
         {description.split('\n').map((paragraph) => (
-          <p className="font-body text-lg mb-2 last:mb-0 ">
+          <p key={paragraph} className="font-body text-lg mb-2 last:mb-0 ">
             {paragraph.trim()}
           </p>
         ))}
+      </section>
+      <section className="prose max-w-prose mb-4">
+        <h2 className="font-title text-4xl tracking-wide">
+          Frequently Asked Questions
+        </h2>
+        {questions.map(({ question, answer }) => (
+          <div class="collapse collapse-arrow font-body" key={question}>
+            <input type="checkbox" />
+            <div class="collapse-title text-xl font-medium">{question}</div>
+            <div class="collapse-content">
+              <p>{answer}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+      <section className="prose max-w-prose mb-4">
+        <h2 className="font-title text-4xl tracking-wide">
+          Still have questions? Contact us!
+        </h2>
+        <form
+          className="card card-bordered flex flex-col bg-slate-700 p-4"
+          onSubmit={async (e) => {
+            e.preventDefault()
+            if (!captchaRef.current) {
+              return
+            }
+            const token = await captchaRef.current.executeAsync()
+            captchaRef.current.render()
+            if (!token) {
+              // TODO: add toast maybe?
+              return
+            }
+            console.log(token)
+            // TODO: add send form logic (backend required)
+          }}
+        >
+          <label className="form-control w-full mb-2">
+            <div class="label">
+              <span class="label-text">What is your name?</span>
+            </div>
+            <input
+              name="name"
+              type="text"
+              required
+              placeholder="Your name"
+              class="input input-bordered input-primary w-full invalid:border-pink-500 invalid:outline-pink-200"
+            />
+          </label>
+          <label className="form-control w-full  mb-2">
+            <div class="label">
+              <span class="label-text">What is your e-mail?</span>
+            </div>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Your email"
+              class="input input-bordered input-primary w-full invalid:border-pink-500 invalid:outline-pink-200"
+            />
+          </label>
+          <label class="form-control w-full mb-4">
+            <div class="label">
+              <span class="label-text">Your question</span>
+            </div>
+            <textarea
+              name="question"
+              class="textarea textarea-primary textarea-bordered h-24 invalid:border-pink-500 invalid:outline-pink-200"
+              placeholder="Your question"
+              required
+            ></textarea>
+          </label>
+          <ReCAPTCHA
+            sitekey="6LdqqU8pAAAAABKZr7HI84xZSRaYXo9o548RAVRH"
+            ref={captchaRef}
+            size="invisible"
+          />
+          <button type="submit" className="btn btn-primary w-full text-xl ">
+            Submit
+          </button>
+        </form>
       </section>
     </div>
   )
--- src/mock-data/text-data.ts
+++ src/mock-data/text-data.ts
@@ -9,6 +9,39 @@ const data = {
   But we can't do it without YOU. Your contribution will not only grant you VIP access to these enlightening sessions but will also empower us to bring these teachings to a wider audience. Together, let's create a community where mindfulness isn't just a practice—it's a way of life.
 
   Join us on this exciting journey, and let's make mindful mastery accessible to everyone, one meditation at a time. Pledge your support today and be a part of the meditation revolution! 🌈🧘‍♀️🚀`,
+  questions: [
+    {
+      question:
+        'Why should I contribute to the "Medito\'s Mindful Mastery" campaign?',
+      answer:
+        'Your contribution to "Medito\'s Mindful Mastery" not only grants you exclusive access to expert-led meditation masterclasses but also plays a crucial role in making these transformative teachings accessible to a global audience. By supporting us, you become an integral part of a movement dedicated to spreading mindfulness and enhancing well-being for everyone, everywhere.',
+    },
+    {
+      question: 'How will my donation be used in this campaign?',
+      answer:
+        'Your generous donation will be primarily used to curate and produce high-quality meditation masterclasses, ensuring that we can bring you insights from top meditation masters around the world. Additionally, it helps us cover production costs, platform maintenance, and outreach efforts to make these classes accessible to as many individuals as possible.',
+    },
+    {
+      question: 'Can I access the masterclasses after the campaign ends?',
+      answer:
+        ' Absolutely! Backers of the "Medito\'s Mindful Mastery" campaign will have exclusive access to the masterclasses even after the campaign concludes. You can revisit these transformative sessions and continue your mindfulness journey at your own pace.',
+    },
+    {
+      question: 'Can I gift the access to masterclasses to someone else?',
+      answer:
+        "Absolutely! If you want to share the gift of mindfulness with a friend or loved one, you can choose a reward tier that includes additional access or simply transfer your access to them after the campaign concludes. It's a wonderful way to spread the joy of meditation.",
+    },
+    {
+      question: 'Will there be live Q&A sessions with the meditation masters?',
+      answer:
+        "Yes! We plan to organize live Q&A sessions where backers can interact directly with the meditation masters, asking questions and gaining personalized insights. It's an exciting opportunity to connect with these experts and deepen your understanding of meditation.",
+    },
+    {
+      question: 'What happens if the funding goal is not reached?',
+      answer:
+        "If, unfortunately, we don't reach our funding goal, all contributions will be refunded, and the \"Medito's Mindful Mastery\" campaign won't proceed. Your support is vital in making this project a reality, so be sure to share the campaign with your network to help us reach our goal!",
+    },
+  ],
 }
 
 export default data
